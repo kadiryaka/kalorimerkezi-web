9e9768c2b4b31bab76a835e9797441070ac8b707
Kadir Yaka <kadiryaka42@gmail.com>
2015-07-15 22:30:11 +0300

    mysql

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: '00125658',
   database: 'kalorimerkezi3'
 });
 
