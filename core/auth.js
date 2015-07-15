4d112b9f013d669f16f18cf4fa5015aabe77aea2
Kadir Yaka <kadiryaka42@gmail.com>
2015-07-16 01:00:41 +0300

    dsf

--- core/auth.js
+++ core/auth.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
+    user: 'kadir',
+    password: '00125658',
     database: 'kalorimerkezi3'
 });
 
